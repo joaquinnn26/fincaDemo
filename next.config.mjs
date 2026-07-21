0e121fa5583772b1da4f2eb7a4518a41ba6ec973
joaquinnn26 <joaquinfefe@gmail.com>
2026-07-21 15:58:59 -0300

    Add GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isGithubPages = process.env.GITHUB_ACTIONS === 'true';
+
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
+  trailingSlash: true,
+  basePath: isGithubPages ? '/fincaDemo' : undefined,
+  assetPrefix: isGithubPages ? '/fincaDemo/' : undefined,
   images: {
     unoptimized: true,
     remotePatterns: [
